5ab82db07eaa088edf6e5607427d74e8662adbfa
mdr396 <eliasbrehanie13@gmail.com>
2026-03-14 17:27:43 +0100

    Add files via upload

---
+++ # Mon travail melange logique, creativit.py
@@ -0,0 +1,74 @@
+# Mon travail melange logique, creativite et organisation. Qui suis-je ?
+# Merci de ne pas descendre voir le code avant d'avoir repondu a la quesion dans le terminal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n="ingenieur"
+nv=input()
+while nv!=n:
+    n="ingenieur"
+    nv=input()
+    if nv != n:
+        print("faux")
+    else:
+        print("c'est gagné direction BUT")
+        break
+# Merci d'avoir participé :)
